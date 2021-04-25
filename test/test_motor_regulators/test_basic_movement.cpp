23ce4d16f76e1e59bca624867086406ebce79e2c
Daniel Armyr <daniel.armyr@frescano.se>
2021-04-25 19:11:44 +0200

    Cleaned up a unit test a bit.

--- test/test_motor_regulators/test_basic_movement.cpp
+++ test/test_motor_regulators/test_basic_movement.cpp
@@ -178,27 +178,26 @@ void testSimpleMoveAtConstantSpeed( unsigned int speedToMoveAt) {
     unsigned long int timeToSettle = 1000;
     unsigned long int endTime = 0;    
 
-    endTime = millis() + timeToMove;
+    
     regulator.setSetPoint( speedToMoveAt );
     regulator.init();            
-
+    
+    //Run to settle the speed.
+    endTime = millis() + timeToSettle;
+    while ( millis() < endTime ) {
+        sched.run();        
+    }
+    
+    //Measure the variance
     float n = 0;
     float S = 0;
     float S2 = 0.0;    
 
-    unsigned long int nextRun = millis() + timeToSettle;
-    while ( true ) {                
-        if ( millis() > endTime ) {
-            unsigned long int speed = encoder.getSpeedCPS();                                    
-            //Log << "Speed: " << speed << " SetPoint: " << speedToMoveAt << endl;
-            TEST_ASSERT_INT_WITHIN( 200, speedToMoveAt, speed );            
-            
-            float stdDev = sqrt( (n*S2-S*S)/(n*(n-1)) );
-            //Log << "Standard deviation: " << stdDev << endl;
-            TEST_ASSERT_FLOAT_WITHIN( 100, 0, stdDev );            
-            return;
-        } else if ( millis() > nextRun ) {
-            nextRun += 10;
+    endTime = millis() + timeToMove;
+    unsigned long int nextRun = millis() + 10;
+    while ( millis() < endTime ) {                                    
+        if ( millis() > nextRun ) {
+            nextRun+=10;
             unsigned long int speed = encoder.getSpeedCPS();                        
             long int speedRel = speed - speedToMoveAt;
             
@@ -208,9 +207,16 @@ void testSimpleMoveAtConstantSpeed( unsigned int speedToMoveAt) {
             S2 += speedRel*speedRel;
             //Log << "speedRel: " << speedRel << " n: " << n << " S: " << S << " S2: " << S2 << endl;
         }
-
-        sched.run();
     }
+
+    unsigned long int speed = encoder.getSpeedCPS();                                    
+    //Log << "Speed: " << speed << " SetPoint: " << speedToMoveAt << endl;
+    TEST_ASSERT_INT_WITHIN( 200, speedToMoveAt, speed );            
+    
+    float stdDev = sqrt( (n*S2-S*S)/(n*(n-1)) );
+    //Log << "Standard deviation: " << stdDev << endl;
+    TEST_ASSERT_FLOAT_WITHIN( 100, 0, stdDev );            
+
 }
 
 void testSimpleMoveAtConstantSpeed7000() {
