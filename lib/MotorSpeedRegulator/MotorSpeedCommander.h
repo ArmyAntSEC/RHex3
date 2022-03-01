aada801c1768739db02d8cce0357c1ac43c67741
Daniel Armyr <daniel.armyr@nextory.com>
2022-03-01 22:37:16 +0100

    More work on the MotorSpeedCommander

--- lib/MotorSpeedRegulator/MotorSpeedCommander.h
+++ lib/MotorSpeedRegulator/MotorSpeedCommander.h
@@ -5,18 +5,40 @@
 class MotorSpeedCommander: public MotorSpeedCommanderInterface
 {
 private:
+    RotationalPositionProvider* currentRotPos;
+    SpeedRegulatorInterface* speedRegulator;
     RotationalPosition positionGoal;
     unsigned long timeGoal;
+    int maxSpeedCPS;
+
+    long cappedSpeedCPS( long speedCPS )
+    {
+        if ( speedCPS < 0 || speedCPS > maxSpeedCPS )
+        {
+            return maxSpeedCPS;
+        } else {
+            return speedCPS;
+        }
+    }
 
 public:
-    void configure(  )
+    void configure( RotationalPositionProvider* _rotPos, SpeedRegulatorInterface* _speedRegulator, int _maxSpeedCPS )
     {
-
+        currentRotPos = _rotPos;
+        speedRegulator = _speedRegulator;
+        maxSpeedCPS = _maxSpeedCPS;
     }
 
     virtual void setGoal( RotationalPosition _pos, unsigned long _time )
     {
         positionGoal = _pos;
         timeGoal = _time;
+    }    
+
+    long computeTargetSpeedCPS( long timeLeftMicros, long clicksLeft )
+    {
+        long int targetSpeedCPS = targetSpeedCPS = (1000L * clicksLeft) / timeLeftMicros;
+
+        return cappedSpeedCPS( targetSpeedCPS );
     }
 };
--- test/testMotorSpeedRegulator/testMotorSpeedCommander.cpp
+++ test/testMotorSpeedRegulator/testMotorSpeedCommander.cpp
@@ -14,9 +14,63 @@ void testSetGoal()
     TEST_ASSERT_EQUAL( 4321, commander.timeGoal );
 }
 
+void testConfigure()
+{
+    RotationalPositionProvider* pos = (RotationalPositionProvider*)1234;
+    SpeedRegulatorInterface* speedRegulator = (SpeedRegulatorInterface*)4321;
+    int maxSpeedCPS = 1234;
+    
+    MotorSpeedCommander commander;
+    commander.configure( pos, speedRegulator, maxSpeedCPS );
+
+    TEST_ASSERT_EQUAL( pos, commander.currentRotPos );    
+    TEST_ASSERT_EQUAL( speedRegulator, commander.speedRegulator );    
+    TEST_ASSERT_EQUAL( maxSpeedCPS, commander.maxSpeedCPS );    
+}
+
+void testComputeTargetSpeed()
+{
+    MotorSpeedCommander commander;   
+    long timeLeftMicros = 1000;    
+    long clicksLeft = 1000;
+    TEST_ASSERT_EQUAL( 1000, commander.computeTargetSpeedCPS( timeLeftMicros, clicksLeft ) );
+}
+
+void testComputeTargetSpeedNegativeTimeLeft()
+{
+    RotationalPositionProvider* pos;
+    SpeedRegulatorInterface* speedRegulator;
+    int maxSpeedCPS = 1234;
+    
+    MotorSpeedCommander commander;
+    commander.configure( pos, speedRegulator, maxSpeedCPS );
+
+    long timeLeftMicros = -1000;    
+    long clicksLeft = 1000;
+    TEST_ASSERT_EQUAL( maxSpeedCPS, commander.computeTargetSpeedCPS( timeLeftMicros, clicksLeft ) );
+}
+
+void testComputeTargetSpeedCapSpeed()
+{
+    RotationalPositionProvider* pos;
+    SpeedRegulatorInterface* speedRegulator;
+    int maxSpeedCPS = 123;
+    
+    MotorSpeedCommander commander;
+    commander.configure( pos, speedRegulator, maxSpeedCPS );
+
+    long timeLeftMicros = 1000;    
+    long clicksLeft = 1000;
+    TEST_ASSERT_EQUAL( maxSpeedCPS, commander.computeTargetSpeedCPS( timeLeftMicros, clicksLeft ) );
+}
+
 void runAllTestsMotorSpeedCommander()
 {
     UNITY_BEGIN_INT();
     RUN_TEST( testSetGoal );
+    RUN_TEST( testConfigure );
+    RUN_TEST( testComputeTargetSpeed );
+    RUN_TEST( testComputeTargetSpeedNegativeTimeLeft );
+    RUN_TEST( testComputeTargetSpeedCapSpeed );
     UNITY_END_INT();
 }
