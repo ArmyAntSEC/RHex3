907c0059f6eb72d889d74b07f80992f49e09c758
Daniel Armyr <daniel.armyr@nextory.com>
2022-04-04 18:53:36 +0200

    Test program now supports two motors. Verified on all three boards

--- src/mainTestMotor.cpp
+++ src/mainTestMotor.cpp
@@ -6,27 +6,31 @@
 #include <BasicEncoder.h>
 
 
-#if 0
-#define MOTOR_EN1 3
-#define MOTOR_EN2 4
-#define MOTOR_PWM 5
 
-#define OPTO      16
-#define ENCODER_2 15
-#define ENCODER_1 14
-#else
-#define MOTOR_EN1 8
-#define MOTOR_EN2 7
-#define MOTOR_PWM 9
+#define MOTOR1_EN1 3
+#define MOTOR1_EN2 4
+#define MOTOR1_PWM 5
 
-#define ENCODER_2 12
-#define ENCODER_1 11
-#define OPTO 10
-#endif
+#define OPTO1      16
+#define ENCODER1_2 14
+#define ENCODER1_1 15
 
-MotorDriver driver;
+#define MOTOR2_EN1 8
+#define MOTOR2_EN2 7
+#define MOTOR2_PWM 9
+
+#define ENCODER2_2 12
+#define ENCODER2_1 11
+#define OPTO2 10
+
+MotorDriver driver1;
+MotorDriver driver2;
 HardwareInterrupts hwInterrupts;
 HardwarePins hwPins;
+
+BasicEncoderFactory encoderFactory;
+BasicEncoder* encoder1;
+BasicEncoder* encoder2;
 
 struct BasicEncoderStore: public BasicEncoderListener
 {    
@@ -35,6 +39,8 @@ struct BasicEncoderStore: public BasicEncoderListener
     volatile int16_t positionAtHoming;
     volatile int32_t timeAtHoming;
     volatile bool newLap = false;
+    int32_t oldLapTime;
+    int32_t oldLapPos;
 
     void signalStepForwardISR()
     {                
@@ -58,13 +64,11 @@ struct BasicEncoderStore: public BasicEncoderListener
         return rValue;
     }   
 
-    void PrintIfNewLap()
+    void PrintIfNewLap( int ID )
     {
         bool newLapExists = false;
-        int32_t newLapTime;
-        static int32_t oldLapTime;
-        int32_t newLapPos;
-        static int32_t oldLapPos;
+        int32_t newLapTime;        
+        int32_t newLapPos;        
 
         hwInterrupts.disableInterrupts();
         if ( newLap && lapCount > 0 ) {
@@ -77,7 +81,7 @@ struct BasicEncoderStore: public BasicEncoderListener
         
         if ( newLapExists )
         {
-            Log << "Position at lap " << lapCount << ": " << newLapPos;
+            Log << "ID: " << ID << " Position at lap " << lapCount << ": " << newLapPos;
             SerialLog << " Lap length: " << newLapPos - oldLapPos;
             SerialLog << " Lap time: " << newLapTime - oldLapTime;
             SerialLog << endl;
@@ -89,9 +93,8 @@ struct BasicEncoderStore: public BasicEncoderListener
      
 };
 
-BasicEncoderStore listener;
-BasicEncoderFactory encoderFactory;
-BasicEncoder* encoder;
+BasicEncoderStore listener1;
+BasicEncoderStore listener2;
 
 void setup()
 {
@@ -101,20 +104,26 @@ void setup()
 
     Serial.println("Hello World!");    
 
-    driver.config( MOTOR_EN1, MOTOR_EN2, MOTOR_PWM, &hwPins );
+    driver1.config( MOTOR1_EN1, MOTOR1_EN2, MOTOR1_PWM, &hwPins );
+    driver2.config( MOTOR2_EN1, MOTOR2_EN2, MOTOR2_PWM, &hwPins );
     
-    encoder = encoderFactory.config<0>( ENCODER_1, ENCODER_2, OPTO, &hwPins );
-    encoder->addListener( &listener );
+    encoder1 = encoderFactory.config<0>( ENCODER1_1, ENCODER1_2, OPTO1, &hwPins );
+    encoder1->addListener( &listener1 );
+    
+    encoder2 = encoderFactory.config<1>( ENCODER2_1, ENCODER2_2, OPTO2, &hwPins );
+    encoder2->addListener( &listener2 );
 
-    driver.setMotorPWM( 128 );
+    driver1.setMotorPWM( 128 );
+    driver2.setMotorPWM( 128 );
 
     int32_t endTime = millis() + 10000;
-    while ( millis() < endTime ) {
-        listener.PrintIfNewLap();
+    while ( millis() < endTime ) {        
+        listener1.PrintIfNewLap(1);
+        listener2.PrintIfNewLap(2);
     }
 
-    driver.setMotorPWM( 0 );
-
+    driver1.setMotorPWM( 0 );
+    driver2.setMotorPWM( 0 );
     
 }
 
